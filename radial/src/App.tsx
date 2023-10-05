56ec7fd74d089dfe4ff5a676c3b12cce19047aaf
Mary Jiang <mvjiang@gmail.com>
2023-10-05 18:20:56 -0400

    setActive item on hover

--- radial/src/App.tsx
+++ radial/src/App.tsx
@@ -15,6 +15,9 @@ function App() {
         <MenuItem title="hello" action={handleAction}/>
         <MenuItem title="testing" action={handleAction}/>
         <MenuItem title="whoop" action={handleAction}/>
+        <MenuItem title="scoop" action={handleAction}/>
+        <MenuItem title="whoop" action={handleAction}/>
+        <MenuItem title="hello" action={handleAction}/>
       </Menu>
     </>
   )
--- radial/src/component/index.tsx
+++ radial/src/component/index.tsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css"
 
 type MenuProps = {
-    children: [];
+    children: React.ReactElement<MenuItemProps>[];
 }
 
 type MenuItemProps = {
     title?: string;
     action?: () => void;
-    index: number;
-    angle: number;
+    index?: number;
+    angle?: number;
+    isActive?: boolean;
 }
 
 export const MenuItem = (props: MenuItemProps) => {
-    const {title, action, index, angle} = props;
+    const {title, action, index, angle, isActive} = props;
 
     return (
-        <li data-pie-item style={{"--pie-item-index": index, "--pie-item-angle": angle+"deg"} as React.CSSProperties} onClick={action}>
+        <li data-pie-item data-pie-item-index={index} data-pie-item-active={isActive} style={{"--pie-item-index": index, "--pie-item-angle": angle+"deg"} as React.CSSProperties}
+        onClick={action}
+        >
             <span data-pie-item-content>{title}</span>
         </li>
     )
@@ -25,11 +28,20 @@ export const MenuItem = (props: MenuItemProps) => {
 const Menu = (props: MenuProps) => {
     const {children} = props;
     const angle = 360 / children.length;
+    const [activeItem, setActiveItem] = useState<number | null>(null);
+
+    const handleSetActive = (e: React.MouseEvent<HTMLUListElement, MouseEvent>) => {
+        const target = e.target as HTMLElement;
+        if (target.hasAttribute("data-pie-item")) {
+            const index = parseInt(target.getAttribute("data-pie-item-index") || "0");
+            setActiveItem(index);
+        }
+    }
 
     return (
-        <ul data-pie-menu>
+        <ul data-pie-menu onMouseMove={(e) => handleSetActive(e)}>
             {children?.map((child, index) => (
-                React.cloneElement(child, {index, angle}) 
+                React.cloneElement(child, {index, angle, isActive: index === activeItem}) 
             ))}
         </ul>
     )
